5e84f8d6533a0ca6588589f27ecf427eba0ca37c
Solomon Lee <squalolee@gmail.com>
2020-08-15 18:35:02 -0400

    Made slight change to upload to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -10,7 +10,9 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
-app.listen(PORT);
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT);
+});
 
 app.get('/',function(req,res){
     res.render('scrape');
